8ac4e485529eb4e8ba74c6476b942641e4cdfa1e
Sebastián T. <98891697+SebastianT454@users.noreply.github.com>
2026-04-07 16:46:08 -0500

    Integracion Jenkinsfile 5

---
+++ src/secret_config.py
@@ -0,0 +1,29 @@
+# Parametros para conexion con la BD (Base de Datos)
+PGHOST='ep-silent-pond-ai7n7vay-pooler.c-4.us-east-1.aws.neon.tech'
+PGDATABASE='neondb'
+PGUSER='neondb_owner'
+PGPASSWORD='npg_mJUDbP2jlk7Q'
+PGSSLMODE='require'
+PGCHANNELBINDING='require'
+PGPORT = 5432 # POR DEFECTO ES 5432, PERO PUEDE CAMBIAR SI NO SE UTILIZA NEON TECH.
+
+# Token para bot de notificaciones.
+NOTIEMAIL = "noreplyredpulse@gmail.com"
+NOTICONTRA = "redpulse.123"
+NOTI_APPCONTRA = "qeip heka pcay mahv"
+
+# Secret key para el apartado web (Flask)
+SECRET_KEY_FLASK = "j98h3y2qweRTY8uiopASDFGHJKLZXCVBNM,./1234567890-=`~!@#$%^&*()_+[]{}|;:?><"
+
+# Key para la conexion con MetaAI # https://groq.com/groqcloud/
+CHAT_BOT_KEY = "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"
+
+# Correo por defecto del administrador
+ADMINEMAIL = "redpulsetst@gmail.com"
+
+# Imagen de perfil por defecto para cada usuario
+DEFAULT_PROFILE_PICTURE = "https://i.pinimg.com/474x/65/25/a0/6525a08f1df98a2e3a545fe2ace4be47.jpg"
+
+# Keys para Imgur
+IMGUR_CLIENT_ID = "a16e9e8cb650a1b"
+IMGUR_CLIENT_SECRET = "dcd1c09918ebcc679c8516672f317ff6c82a8ece"
